9f29d1e049352e4b897b32109ad3087af70ceb6b
Janine Luk <janineluk95@gmail.com>
2018-11-18 11:11:09 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
   user_guess = gets.chomp
   while user_guess == "exit"
     
-    if user_guess == random_guess.to_f
+    if user_guess == random_guess.to_s
       puts "You guessed the correct number!"
       run_guessing_game
     
