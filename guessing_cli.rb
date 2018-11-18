996e3ddea0f62fe4a8356e252e3977666e9ec78c
Janine Luk <janineluk95@gmail.com>
2018-11-18 11:16:11 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,8 +4,8 @@ def run_guessing_game
   puts "Guess a number between 1 and 6:"
   user_guess = gets.chomp
     if user_guess == "exit"
-      break
       puts "Goodbye"
+      break
     elsif user_guess == random_guess.to_s 
       puts "You guessed the number correct!"
     elsif user_guess == 
