4bcd31b5d85689ac1787f0d1dab6525f9ccbf3ce
Janine Luk <janineluk95@gmail.com>
2018-11-18 11:19:31 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
      if user_guess == "exit"
       break
       elsif user_guess == random_guess.to_s 
-        puts "You guessed the number correct!"
+        puts "You guessed the correct number!"
       elsif !(user_guess == random_guess.to_s)
         puts "The computer guessed #{random_guess}."  
       end
